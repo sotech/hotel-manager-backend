996ce124e39fc869623b141f4b0bd17d33505ac5
German Sommariva <germansommariva@gmail.com>
2022-03-28 01:38:09 -0300

    Morgan

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
 import mongo from "./utils/mongoConnection";
+import morgan from 'morgan';
+import userRoutes from "./routes/user";
 
 const app = express();
-import userRoutes from "./routes/user";
 const port = process.env.PORT || 8000
-
+app.use(morgan('dev'));
 app.use(express.json());
 app.use("/user", userRoutes);
 
